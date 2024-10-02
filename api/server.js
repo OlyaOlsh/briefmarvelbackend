2f46035f8e308329a133ad578b592b8bfb13d54c
olgaolsh <oliprogram@gmail.com>
2024-10-02 18:46:10 +0300

    и

--- api/server.js
+++ api/server.js
@@ -2,24 +2,25 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
 
 const token = process.env.TOKEN;
+if (!token) {
+    throw new Error('Telegram Bot Token not provided!');
+}
 const groupChatId = process.env.GROUP_CHAT_ID;
-const webAppUrl = process.env.WEB_APP_URL;
 
+// Создаем экземпляр бота с включенным polling
 const bot = new TelegramBot(token, { polling: true });
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes(bot, groupChatId)); // Подключаем маршруты
+
+// Подключаем маршруты
+app.use('/api', require('./routes')(bot, groupChatId)); // Передаем bot и groupChatId
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
-
-// Экспортируем приложение для использования в index.js
-module.exports = app;
